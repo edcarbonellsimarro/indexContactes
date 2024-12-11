eecafad0578aa5063b5e1c7cfcfe50ffb198c2d3
edcarbonellsimarro <ecarbonellsimarro@gmail.com>
2024-12-11 12:29:38 +0100

    PlantillaHerrencia

--- contactesPlantilles/src/Controller/IniciController.php
+++ contactesPlantilles/src/Controller/IniciController.php
@@ -2,12 +2,13 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class IniciController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class IniciController extends AbstractController
 {
 	#[Route('/' ,name:'inici')]
 	public function inici()
 	{
-		return new Response("Benvinguda al web de contactes");
+		return $this->render('inici.html.twig');
 	}
 }
 ?>
