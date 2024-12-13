faac0cb78ef06a5b8caeaf4a46747d6e2696a996
edcarbonellsimarro <ecarbonellsimarro@gmail.com>
2024-12-13 08:28:40 +0100

    IniciServeis

---
+++ contactesServeis/src/Controller/IniciController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class IniciController extends AbstractController
+{
+	#[Route('/' ,name:'inici')]
+	public function inici()
+	{
+		return $this->render('inici.html.twig');
+	}
+
+	#[Route('/iniciHerencia' ,name:'iniciHerencia')]
+	public function iniciHerencia()
+	{
+		return $this->render('iniciHerencia.html.twig');
+	}
+}
+?>
